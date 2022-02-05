9ef1184dc055c904fc7cf833d48395f54806b561
benjamintenny <98218460+benjamintenny@users.noreply.github.com>
2022-02-05 19:19:20 +0300

    Create dummy.py

---
+++ webapp/dummy.py
@@ -0,0 +1,25 @@
+text = '''
+Section "Device"
+    Identifier  "Configured Video Device"
+    Driver      "dummy"
+EndSection
+
+Section "Monitor"
+    Identifier  "Configured Monitor"
+    HorizSync 31.5-48.5
+    VertRefresh 50-70
+EndSection
+
+Section "Screen"
+    Identifier  "Default Screen"
+    Monitor     "Configured Monitor"
+    Device      "Configured Video Device"
+    DefaultDepth 24
+    SubSection "Display"
+    Depth 24
+    Modes "1024x800"
+    EndSubSection
+EndSection
+'''
+with open("/usr/share/X11/xorg.conf.d/xorg.conf", "w+") as f:
+  f.write(text)
